ec9048e1782f685ee624b0eeb4dc8176ade31a63
Joshua Goldberg <joshuabg@terpmail.umd.edu>
2019-03-30 09:10:09 -0400

    Import example navigation code

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,43 @@
 #include <Arduino.h>
+#include <Enes100Simulation.h>
+#include <TankSimulation.h>
 
 void setup() {
-  // put your setup code here, to run once:
+  TankSimulation.begin();
+  while (!Enes100Simulation.begin()) {
+    Enes100Simulation.println("Unable to connect to simulation");
+  }
+
+  Enes100Simulation.println("Starting Navigation");
+
+  while (!Enes100Simulation.updateLocation()) {
+    Enes100Simulation.println("Unable to update Location");
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // turn to face forward
+  while (abs(Enes100Simulation.location.theta) > 0.05) {
+    TankSimulation.setLeftMotorPWM(255);
+    TankSimulation.setRightMotorPWM(-255);
+
+    while (!Enes100Simulation.updateLocation()) {
+      Enes100Simulation.println("Unable to update location");
+    }
+  }
+
+  // move forward
+  TankSimulation.setLeftMotorPWM(255);
+  TankSimulation.setRightMotorPWM(255);
+
+  while (Enes100Simulation.readDistanceSensor(1) > 0.2) {
+    Enes100Simulation.println("No obstacle seen within 0.2 meters");
+  }
+
+  // stop once an obstacle is seen
+  TankSimulation.turnOffMotors();
+  Enes100Simulation.println("Found Obstacle");
+
+  while (1)
+    ;
 }
