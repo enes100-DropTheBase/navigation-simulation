4c8499f545aef59a03915fccb081fe984bc34f9f
Joshua Goldberg <joshuabg@terpmail.umd.edu>
2019-03-30 10:02:50 -0400

    Add basic method to get to destination

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,9 @@
 #include <Enes100Simulation.h>
 #include <TankSimulation.h>
 
+double getAngleToDest();
+double getDistToDest();
+
 void setup() {
   TankSimulation.begin();
   while (!Enes100Simulation.begin()) {
@@ -16,28 +19,84 @@ void setup() {
 }
 
 void loop() {
-  // turn to face forward
-  while (abs(Enes100Simulation.location.theta) > 0.05) {
-    TankSimulation.setLeftMotorPWM(255);
-    TankSimulation.setRightMotorPWM(-255);
+  double targetAngle = getAngleToDest();
 
-    while (!Enes100Simulation.updateLocation()) {
-      Enes100Simulation.println("Unable to update location");
+  if (getDistToDest() > 0.1) {
+    if (Enes100Simulation.location.x > Enes100Simulation.destination.x) {
+      if (targetAngle < 0) {
+        targetAngle += PI;
+      } else {
+        targetAngle -= PI;
+      }
     }
+
+    Enes100Simulation.print("Distance to destination: ");
+    Enes100Simulation.println(getDistToDest());
+
+    Enes100Simulation.print("Target Angle: ");
+    Enes100Simulation.println(targetAngle * 180 / PI);
+
+    // turn to face destination
+    while (abs(Enes100Simulation.location.theta - targetAngle) > 0.05) {
+      if (Enes100Simulation.location.theta - targetAngle > 0) {
+        TankSimulation.setLeftMotorPWM(255);
+        TankSimulation.setRightMotorPWM(-255);
+      } else {
+        TankSimulation.setLeftMotorPWM(-255);
+        TankSimulation.setRightMotorPWM(255);
+      }
+
+      while (!Enes100Simulation.updateLocation()) {
+        Enes100Simulation.println("Unable to update location");
+      }
+    }
+
+    // move forward
+    TankSimulation.setLeftMotorPWM(255);
+    TankSimulation.setRightMotorPWM(255);
+
+    Enes100Simulation.println(Enes100Simulation.readDistanceSensor(1));
+
+    if (Enes100Simulation.readDistanceSensor(1) > 0.1 ||
+        Enes100Simulation.readDistanceSensor(1) == 0) {
+      if (getDistToDest() < 0.5) {
+        delay(100);
+      } else {
+        delay(1000);
+      }
+    }
+
+    // stop
+    TankSimulation.setLeftMotorPWM(0);
+    TankSimulation.setRightMotorPWM(0);
   }
+}
 
-  // move forward
-  TankSimulation.setLeftMotorPWM(255);
-  TankSimulation.setRightMotorPWM(255);
+double getAngleToDest() {
+  // TODO: add checks to make sure location can be updated
+  Enes100Simulation.updateLocation();
+  double deltaX =
+      Enes100Simulation.location.x - Enes100Simulation.destination.x;
+  double deltaY =
+      Enes100Simulation.location.y - Enes100Simulation.destination.y;
 
-  while (Enes100Simulation.readDistanceSensor(1) > 0.2) {
-    Enes100Simulation.println("No obstacle seen within 0.2 meters");
-  }
+  // Enes100Simulation.println(deltaX);
+  // Enes100Simulation.println(deltaY);
 
-  // stop once an obstacle is seen
-  TankSimulation.turnOffMotors();
-  Enes100Simulation.println("Found Obstacle");
+  double angle = atan(deltaY / deltaX);
 
-  while (1)
-    ;
+  // Enes100Simulation.println(angle);
+
+  return angle;
+}
+
+double getDistToDest() {
+  // TODO: add checks to make sure location can be updated
+  Enes100Simulation.updateLocation();
+  double deltaX =
+      Enes100Simulation.location.x - Enes100Simulation.destination.x;
+  double deltaY =
+      Enes100Simulation.location.y - Enes100Simulation.destination.y;
+
+  return sqrt(pow(deltaX, 2) + pow(deltaY, 2));
 }
